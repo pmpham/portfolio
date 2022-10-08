18477fb2aacdebb50697777e045238ecfd402a67
pmpham <petermphamm@gmail.com>
2022-10-07 23:25:11 -0700

    update alt props again

--- src/About.js
+++ src/About.js
@@ -8,7 +8,7 @@ import Desc from "./data/about.json";
 const About = () => {
  return (
   <div className="about_div">
-   <img className="picture" src="peter.png" alt="picture of my sexy face" style={{padding:5}}></img>
+   <img className="picture" src="peter.png" alt="my sexy face" style={{padding:5}}></img>
    <Card className="about_card" inset={true} maxWidth={360}>
     <h3 className="name">peter pham</h3>
     {Desc.map((desc) => {
