83c48ee6ed900e0a1bd4fe532d1e75f923e4d776
pmpham <petermphamm@gmail.com>
2023-01-24 00:18:07 -0800

    added basic routes

--- src/App.js
+++ src/App.js
@@ -4,20 +4,20 @@ import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import Experience from "./Experience";
 import Projects from "./Projects";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Route, Routes} from "react-router-dom";
 
 function App() {
  return (
-  <Router>
+  <main>
     <Navbar />
-   <main>
+   <BrowserRouter>
     <Routes>
      <Route exact path="/" element={<Home/>} />
      <Route path="/experience" element={<Experience/>} />
      <Route path="/projects" element={<Projects/>} />
     </Routes>
-   </main>
-  </Router>
+   </BrowserRouter>
+  </main>
  );
  /* return (
     <div className="App">
